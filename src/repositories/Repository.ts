5d0ba5672e8946147ad9de2e4dd47923c30c8b33
Amanda Oliveira <amanda.oliveira@dtidigital.com.br>
2022-02-13 22:49:11 -0300

    repositório async

--- src/repositories/Repository.ts
+++ src/repositories/Repository.ts
@@ -9,82 +9,48 @@ class Repository {
   }
 
   async listarPedidos(): Promise<IPedidoCompleto[]> {
-    // instancia numa variavel isso aq axios.get<IPedidoCompelto[]>("URL");
-    // const response = axios.get("http://localhost:3333/");
-    // return response.data;
-    return [
+    const response = await axios.get("http://localhost:3333/");
+    return response.data;
+  }
+
+  async inserirItemCardapio(item: IRefeicao) {
+    const response = await axios.post("http://localhost:3333/", {
+      name: item.nomeRefeicao,
+      description: item.descricaoRefeicao,
+      calories: item.calorias,
+      price: item.valorRefeicao,
+      quantity: item.quantidadeRefeicao,
+    });
+    return response.data;
+  }
+
+  async removerItemCardapio(item: IRefeicao) {
+    const response = await axios.delete(
+      `http://localhost:3333/${item.idRefeicao}`
+    );
+    return response.data;
+  }
+
+  async editarItemCardapio(item: IRefeicao) {
+    const response = await axios.put(
+      `http://localhost:3333/${item.idRefeicao}`,
       {
-        idPedido: 12,
-
-        cliente: {
-          nome: "Amanda",
-          cpf: 13967450686,
-          endereco: {
-            cep: 32315040,
-            logradouro: "José faria",
-            numero: 1900,
-            complemento: "402",
-          },
-        },
-        refeicao: [
-          {
-            idRefeicao: 1,
-            nomeRefeicao: "feijoada",
-            descricaoRefeicao: "gostosa",
-            valorRefeicao: 50,
-            quantidadeRefeicao: 1,
-            calorias: 450,
-          },
-          {
-            idRefeicao: 2,
-            nomeRefeicao: "aiai",
-            descricaoRefeicao: "gostosa",
-            valorRefeicao: 40,
-            quantidadeRefeicao: 1,
-            calorias: 440,
-          },
-          {
-            idRefeicao: 3,
-            nomeRefeicao: "dsf",
-            descricaoRefeicao: "gostosa",
-            valorRefeicao: 32,
-            quantidadeRefeicao: 1,
-            calorias: 20,
-          },
-        ],
-        idFormaPagamento: 1,
-      },
-    ];
+        name: item.nomeRefeicao,
+        description: item.descricaoRefeicao,
+        calories: item.calorias,
+        price: item.valorRefeicao,
+        quantity: item.quantidadeRefeicao,
+      }
+    );
+    return response.data;
   }
 
-  inserirItemCardapio(item: IRefeicao) {
-    axios.post("http://localhost:3333/", {
-      name: item.nomeRefeicao,
-      description: item.descricaoRefeicao,
-      calories: item.calorias,
-      price: item.valorRefeicao,
-      quantity: item.quantidadeRefeicao,
-    });
-    console.log("item adicionado com sucesso", item);
-  }
-  removerItemCardapio(item: IRefeicao) {
-    axios.delete(`http://localhost:3333/${item.idRefeicao}`);
-
-    console.log("item removido com sucesso", item);
-  }
-  editarItemCardapio(item: IRefeicao) {
-    axios.put(`http://localhost:3333/${item.idRefeicao}`, {
-      name: item.nomeRefeicao,
-      description: item.descricaoRefeicao,
-      calories: item.calorias,
-      price: item.valorRefeicao,
-      quantity: item.quantidadeRefeicao,
-    });
-    console.log("item editado com sucesso", item);
-  }
-  enviarPedido(pedido: IPedidoCompleto) {
-    axios.post("http://localhost:3333/", pedido.idPedido);
-    console.log("pedido enviad", pedido);
+  async enviarPedido(pedido: IPedidoCompleto) {
+    const response = await axios.post(
+      "http://localhost:3333/",
+      pedido.idPedido
+    );
+    return response.data;
   }
 }
 
